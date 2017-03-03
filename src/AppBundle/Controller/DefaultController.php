cb02e3be5154daef6a0f002795b4a3f805e6e6ee
David Amigo <david.amigo@privalia.com>
2017-03-03 13:54:41 +0100

    Create a controller to test the recursive division algorithm

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,10 +2,17 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Maze;
+use AppBundle\Service\MazeBuilder\MazeBuilderRecursiveDivision;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Default Controller
+ *
+ * @package AppBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
@@ -16,6 +23,19 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/maze/test", name="maze-test")
+     */
+    public function mazeTestAction()
+    {
+        $builder = new MazeBuilderRecursiveDivision();
+        $maze = $builder->buildRandomMaze(40, 20);
+
+        return $this->render('default/maze-test.html.twig', [
+            'maze' => $maze
         ]);
     }
 }
